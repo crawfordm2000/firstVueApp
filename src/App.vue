3f26a1922c6d0f7007069c7d6cb352bfbb9a4989
crawfordm2000 <crawfordm2000@gmail.com>
2020-02-03 10:30:53 -0500

    added embedded video player

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container">
         <SearchBar @termChange="onTermChange"></SearchBar>
-        <VideoList :videos="videos"></VideoList>
+        <div class="row">
+            <VideoDetail :video="selectedVideo" />
+            <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
     </div> 
 </template>
 
@@ -11,21 +14,27 @@
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail'
 
 
 
 const API_KEY = process.env.VUE_APP_API_KEY;
 
+//process.env.VUE_APP_API_KEY;
 export default {
     name: 'App',
     components: {
         SearchBar,
-        VideoList
+        VideoList,
+        VideoDetail
     },
     data() {
-        return { videos: [] } 
+        return { videos: [], selectedVideo: null } 
     },
     methods: {
+        onVideoSelect(video){
+            this.selectedVideo = video;
+        },
         onTermChange(searchTerm) {
             axios.get('https://www.googleapis.com/youtube/v3/search', {
                 params: {
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,9 +1,10 @@
 <template>
-    <ul class="list-group">
+    <ul class="list-group col-md-4">
       <VideoListItem 
         v-for="video in videos"
         :video="video"
         :key="video.etag"
+        @videoSelect="onVideoSelect"
         >
       </VideoListItem>
       
@@ -20,9 +21,12 @@ export default {
    components: {
        VideoListItem
    },
-   props: { // or ['videos'] = without validation
-       videos: Array
-   },
+   props: ['videos'],
+   methods: {
+     onVideoSelect(video) {
+       this.$emit('videoSelect', video);
+     }
+   }
    
 };
 </script>
